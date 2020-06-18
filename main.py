c61d254c2a82bc366e117e3dbb855f8281e571e6
Vidar <zingmarkvidar@gmail.com>
2020-06-18 12:15:11 +0200

    main.py

--- main.py
+++ main.py
@@ -2,23 +2,25 @@ from bs4 import BeautifulSoup
 import requests
 
 
-def apply():
+def applyIndeed():
 
+    appliers = []
     jobTitlesActive = {'Testare', 'Utvecklare', 'Testautomatiserare'}
-
 
     sourcePage = requests.get("https://se.indeed.com/Testare-jobb-i-Stockholms-L%C3%A4n").text
     sourceCode = BeautifulSoup(sourcePage, 'lxml')
 
     for jobs in sourceCode.find_all('div', 'jobsearch-SerpJobCard unifiedRow row result'):
-        content = jobs.text
-        for jobtitle in jobTitlesActive:
-            str(content).__contains__(jobtitle)
 
+        for elements in jobs.contents:
+            for jobtitle in jobTitlesActive:
+                if hasattr(elements, 'text'):
+                    if str(elements.text).__contains__(jobtitle):
+                        jobID = jobs.attrs.get('id')  # Gets the job id so it can parse it to selenium
+                        appliers.append(jobID)
 
-
-    sourceCode.find_all('div', 'jobsearch-SerpJobCard unifiedRow row result')
+    return appliers
 
 
 if __name__ == '__main__':
-    apply()
+    print(applyIndeed())
