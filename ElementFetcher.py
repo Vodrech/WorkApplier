a41efc400b6b0bd18aa3d830299ee6c74a33926d
Vidar <zingmarkvidar@gmail.com>
2020-06-21 19:23:41 +0200

    main.py

--- ElementFetcher.py
+++ ElementFetcher.py
@@ -1,21 +1,28 @@
 from SeleniumObject import SeleniumObject
 from selenium.webdriver.common.keys import Keys
+import main
 import time
 
 
 class ElementFetcher:
 
-    SB = SeleniumObject("https://se.indeed.com/?from=gnav-homepage", "Testare", "Stockholms Län")
 
-    WorkRole = SB.driver.find_element_by_id("text-input-what")
-    Destination = SB.driver.find_element_by_id("text-input-where")
+    def test(self):
+        SB = SeleniumObject("https://se.indeed.com/?from=gnav-homepage", "Testare", "Stockholms Län")
 
-    WorkRole.send_keys(SB.role)
-    Destination.send_keys(SB.destination, Keys.ENTER)
+        WorkRole = SB.driver.find_element_by_id("text-input-what")
+        Destination = SB.driver.find_element_by_id("text-input-where")
 
-    JobCard = SB.driver.find_element_by_css_selector("div[class='jobsearch-SerpJobCard unifiedRow row result clickcard']")
+        WorkRole.send_keys(SB.role)
+        Destination.send_keys(SB.destination, Keys.ENTER)
+
+        JobCard = SB.driver.find_element_by_css_selector("div[class='jobsearch-SerpJobCard unifiedRow row result clickcard']")
 
 
+        time.sleep(2)
+
+    listYEE = main.get_workplace_webpage()
+    SeleniumObject(listYEE[0], "Testare", "Stockholms Län")
     time.sleep(2)
 
 
--- main.py
+++ main.py
@@ -1,26 +1,87 @@
 from bs4 import BeautifulSoup
 import requests
+import json
+
+dictionary = {
+    'sourcePage': 'https://se.indeed.com/Testare-jobb-i-Stockholms-L%C3%A4n'
+}
+
+sourcePage = requests.get(dictionary.get('sourcePage')).text
+sourceCode = BeautifulSoup(sourcePage, 'lxml')
 
 
-def applyIndeed():
+def get_active_jobs():  # Methods that searches through the job results on indeed.com and verifies it contains the job roles that is selected.
 
     appliers = []
-    jobTitlesActive = {'Testare', 'Utvecklare', 'Testautomatiserare'}
-
-    sourcePage = requests.get("https://se.indeed.com/Testare-jobb-i-Stockholms-L%C3%A4n").text
-    sourceCode = BeautifulSoup(sourcePage, 'lxml')
-
+    jobTitles = {"Testare", "Utvecklare", "TestAutomatiserare"}
     for jobs in sourceCode.find_all('div', 'jobsearch-SerpJobCard unifiedRow row result'):
 
         for elements in jobs.contents:
-            for jobtitle in jobTitlesActive:
+            for jobTitle in jobTitles:
                 if hasattr(elements, 'text'):
-                    if str(elements.text).__contains__(jobtitle):
-                        jobID = jobs.attrs.get('id')  # Gets the job id so it can parse it to selenium
+                    if str(elements.text).__contains__(jobTitle):
+                        jobID = jobs.attrs.get('id')  # Gets the job id so it can parse it to get_applies
                         appliers.append(jobID)
 
+    print('Method:', get_active_jobs.__name__, '\nreturned a list of : ', len(appliers))
     return appliers
 
 
-if __name__ == '__main__':
-    print(applyIndeed())
+def get_applies():  # Method that finds the link to the jobs.
+
+    jobList = get_active_jobs()
+    job_links = []
+
+    for job in jobList:#Loops through each job and gets the apply page.
+
+        for selectData in sourceCode.find_all('div', {'id': job}):  # Finds each div with id element equal to 'job'
+            if hasattr(selectData, 'contents'):
+
+                for s in selectData.contents:
+                    if hasattr(s, 'attrs'):
+
+                        attributes = dict(s.attrs)
+                        if 'class' in attributes.keys():
+                            if 'title' in attributes.get('class'):
+
+                                for a in s.contents:
+                                    if hasattr(a, 'attrs'):
+
+                                        attributes2 = dict(a.attrs)
+                                        if 'href' in attributes2.keys():
+
+                                            link = "https://se.indeed.com" + attributes2.get('href')
+                                            job_links.append(link)
+
+    print('Method:', get_applies.__name__, '\nfetched:', len(jobList), '/', len(job_links))
+
+    return job_links
+
+
+def get_workplace_webpage():  # Gets the work recruiter web-page.
+
+    links = get_applies()
+    workplace_webpages = []
+
+    for link in links:
+        sourcePage2 = requests.get(link).text
+        sourceCode2 = BeautifulSoup(sourcePage2, 'lxml')
+
+        for webpage in sourceCode2.find_all('div', 'icl-u-lg-hide'):
+            if hasattr(webpage, 'attrs'):
+
+                attributes = dict(webpage.attrs)
+                if 'class' in attributes.keys():
+
+                    if attributes.get('class').__len__() == 1:
+                        if hasattr(webpage, 'contents'):
+
+                            for content in webpage.contents:
+                                if hasattr(content, 'attrs'):
+
+                                    attributes2 = dict(content.attrs)
+                                    if 'href' in attributes2.keys():
+                                            link = attributes2.get('href')
+                                            workplace_webpages.append(link)
+
+    return workplace_webpages
